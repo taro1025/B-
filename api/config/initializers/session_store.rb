00aae51e18a5c0ad7196e3c40b97e7b25055a0ac
taro1025 <game4967@gmail.com>
2021-06-13 09:17:03 +0900

    Initialized

---
+++ api/config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env === 'production'
+    Rails.application.config.session_store :cookie_store, key: '_dhioru', domain: 'フロントエンドのドメイン'
+else
+    Rails.application.config.session_store :cookie_store, key: '_dhioru', secure: false
+end
