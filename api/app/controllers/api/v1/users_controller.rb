a07fafb447882e2da197ae5a659ad42b12f15ec7
taro1025 <game4967@gmail.com>
2021-06-13 11:26:40 +0900

    Comment out docker-compose.yml depend of web

---
+++ api/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        render json: {}, status: :ok
+      end
+
+      def show
+        render json: {}, status: :ok
+      end
+
+      def create
+        render json: {}, status: :ok
+      end
+
+      def destroy
+        render json: {}, status: :ok
+      end
+
+      def edit
+        render json: {}, status: :ok
+      end
+    end
+  end
+end
