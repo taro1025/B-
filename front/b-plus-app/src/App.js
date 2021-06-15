f95588304498ed11d89cc56cb7e20216b62ec548
taro1025 <game4967@gmail.com>
2021-06-15 19:21:10 +0900

    tes

--- front/b-plus-app/src/App.js
+++ front/b-plus-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactooooooo
+          Learn Reactooooooopai
         </a>
       </header>
     </div>
