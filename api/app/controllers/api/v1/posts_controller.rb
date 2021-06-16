d9a874ff4284c78261756758cfcd2f0660a219b6
taro1025 <game4967@gmail.com>
2021-06-16 20:28:06 +0900

    Add Update Action

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class PostsController < ApplicationController
-      before_action :correct_user, only: [:create, :destroy]
+      before_action :correct_user, only: [:create, :destroy, :update]
 
       def create
         post = @user.posts.new(book_isbn: params[:book_isbn], impression: params[:impression])
@@ -18,6 +18,16 @@ module Api
           render json: {}, status: :ok
         else
           render json: { message: "投稿の削除に失敗しました。"}, status: :internal_server_error
+        end
+      end
+
+      def update
+        post = Post.find(params[:post_id])
+        if post
+          post.update_attribute(:impression, params[:impression])
+          render json: { post: post}, status: :ok
+        else
+          render json: { message: "投稿の編集に失敗しました。"}, status: :internal_server_error
         end
       end
 
--- api/spec/requests/posts_spec.rb
+++ api/spec/requests/posts_spec.rb
@@ -23,10 +23,29 @@ RSpec.describe "Posts", type: :request do
 
       expect{
         delete "/api/v1/posts/#{@user.id}", params: {
-                    id: @user.id, post_id: res['post']['id']
+                    post_id: res['post']['id']
         }
       }.to change{ @user.posts.count }.by(-1)
     end
   end
 
+  describe "can be edited" do
+    it "should be rewite" do
+      #Login
+      post "/api/v1/login", params: { session: { email: @user.email,password: @user.password}}
+      #Create
+      post "/api/v1/posts", params: {
+                    id: @user.id, book_isbn: "0123456789",
+                    impression: "とても面白かった"
+                    }
+      res = JSON.parse(response.body)
+      patch "/api/v1/posts/#{@user.id}", params: {
+                    post_id: res['post']['id'],
+                    impression: "非常に面白かった"
+                    }
+
+      post = Post.find(res['post']['id'])
+      expect(post.impression).to eq("非常に面白かった")
+    end
+  end
 end
