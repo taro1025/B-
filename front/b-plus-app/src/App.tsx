bfd893aa1e838058e3a1cc96dcbf5a9098a8646e
taro1025 <game4967@gmail.com>
2021-06-17 18:53:14 +0900

    Create react-app with TS

--- front/b-plus-app/src/App.tsx
+++ front/b-plus-app/src/App.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactooooooopai
+          Learn React
         </a>
       </header>
     </div>
