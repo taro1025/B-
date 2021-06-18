beb8c81a446041d7b43cbed95be42058a43cc10b
taro1025 <game4967@gmail.com>
2021-06-19 06:34:58 +0900

    Add db seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  u = User.create(name: "testman#{n}", email: "titi#{n}@gmail.com",
+                  password: "testes1234", password_confirmation: "testes1234")
+  u.posts.create(book_isbn:  "#{n}123456789", impression: "面白かった#{n}")
+  u.book_user_reads.create(book_isbn: "#{n}123456789")
+  u.book_want_to_reads.create(book_isbn: "#{n}123456789")
+  u.ranks.create(book_isbn: "#{n}123456789", rank_id: 3)
+  u.book_user_favorites.create(book_isbn: "#{n}123456789", description_summary: "マジやばい#{n}")
+end
