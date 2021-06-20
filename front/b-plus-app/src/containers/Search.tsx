5855f430ceb451fec944aad86935ff94948eff74
taro1025 <game4967@gmail.com>
2021-06-21 07:51:30 +0900

    googleAPiにisbnがなかったのでタミーデータを用意

--- front/b-plus-app/src/containers/Search.tsx
+++ front/b-plus-app/src/containers/Search.tsx
@@ -3,22 +3,45 @@ import TextField from '@material-ui/core/TextField';
 import { searchBooks } from "../apis/searchBooks"
 import { NoDecoLink } from "../components/NoDecoLink";
 import { bookStateProps } from "../interfaces"
+import packagelock from "../package-lock.jpeg" //ダミー画像
 
 
 export const Search = (props: bookStateProps) => {
 
   const [word, setWord] = useState<string>("")
-  const books = props.books
+  let books = props.books
   const setBooks = props.setBooks
+  //このbooksデータはダミー。isbnがgoogleapiになかったので。
+  books = [
+
+    [{
+      isbn: "0123456789",
+      title: "進撃の巨人1"
+    }],
+    [{
+      isbn: "1123456789",
+      title: "進撃の巨人1"
+    }],
+    [{
+      isbn: "2123456789",
+      title: "進撃の巨人1"
+    }],
+    [{
+      isbn: "3123456789",
+      title: "進撃の巨人1"
+    }]
+  ]
+  console.log(books)
+  //setBooks(books)
+
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     console.log("これから検索")
     searchBooks(word)
       .then(res => {
-        setBooks(res.items)
-        console.log("res items", res.items)
-        console.log("setできてるか", books)
+        //setBooks(res.items)
+        //console.log(books)
       })
   }
 
@@ -43,15 +66,16 @@ export const Search = (props: bookStateProps) => {
       {books && (
         <div>
           {books.map((item: any, i: number) => {
+            console.log("item", item)
             return (
-              item.volumeInfo.imageLinks ?
-                <div key={item.id}><NoDecoLink to={`/book/${i}`}><img src={item.volumeInfo.imageLinks.smallThumbnail} /></NoDecoLink></div>
-                :
-                <p>noimage</p>)
+
+              < div key={item.id} > <NoDecoLink to={`/book/${item[0].isbn}`}><img src={packagelock}></img></NoDecoLink></div>
+            )
           })}
         </div>
-      )}
+      )
+      }
 
-    </div>
+    </div >
   )
 }
