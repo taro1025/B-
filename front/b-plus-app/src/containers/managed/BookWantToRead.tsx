3b4f1e7f7acf5618c78f507e8e989dfa32ca4690
taro1025 <game4967@gmail.com>
2021-06-21 20:11:40 +0900

    読みたい本pageできた

--- front/b-plus-app/src/containers/managed/BookWantToRead.tsx
+++ front/b-plus-app/src/containers/managed/BookWantToRead.tsx
@@ -1,33 +1,65 @@
 import { useEffect, useContext, useState } from "react"
-import { User } from "../../App"
-import { fetchBookWantToRead } from "../../apis/fetchBookWantToRead"
+import { User } from "../../App";
+import { fetchBookWantToRead } from "../../apis/fetchBookWantToRead";
+import styled from 'styled-components';
+import { NoDecoLink } from "../../components/NoDecoLink";
+import dummyImage from "../../dummyImage.jpeg"
 
+const Ul = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+`
+
+const Li = styled.li`
+  flex-basis: 33.3%;
+`
+
+const Img = styled.img`
+  width: 100%;
+  border: 1px solid #fff;
+`
+
+//ダミーデータを使用のためコメントアウトしてる
 export const BookWantToRead = () => {
 
-  const [books, setBooks] = useState<any>()
+  //const [books, setBooks] = useState<any>()
 
   const context: any = useContext(User)
 
-  //res.booksは本のisbnが複数入ってる。本当はこれを使ってAmazonAPIで本の情報を取得したい。
+  const dummy_books: any = []
+  for (let i = 0; i < 20; i++) {
+    dummy_books.push(
+      <Li><NoDecoLink to={`/book/9999999`}><Img src={dummyImage} /></NoDecoLink></Li>
+    )
+  }
+  //res.booksは本のisbnが複数入ってる。本当はさらにこれを使ってAmazonAPIで本の情報を取得したい。
   useEffect(() => {
-    fetchBookWantToRead(context.user.id)
-      .then(res => {
-        console.log("response", res.books)
-        setBooks(res.books)
-      })
+    if (context.user) {
+      fetchBookWantToRead(context.user.id)
+        .then(res => {
+          //console.log("response", res.books)
+          //setBooks(res.books)
+        })
+    }
   }, [])
 
   return (
-    <div>
-      <p>読見たい本のページ</p>
+    <Ul>
       {
-        books &&
-
-        books!.map((item: any) => {
-          console.log("アイテム", item)
-          return <p>{item.book_isbn}</p>
+        dummy_books.map((book: any) => {
+          return book
         })
       }
-    </div>
+      {
+        //books &&
+        //
+        //books!.map((item: any) => {
+        //  console.log("アイテム", item)
+        //  return <p>{item.book_isbn}</p>
+        //})
+      }
+    </Ul>
   )
 }
