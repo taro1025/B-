8cf030f0fc96ed76ebf87db5a7ba37fb5d4c1421
taro1025 <game4967@gmail.com>
2021-06-22 18:48:01 +0900

    感想の型を少し

--- front/b-plus-app/src/dummyData.tsx
+++ front/b-plus-app/src/dummyData.tsx
@@ -23,26 +23,31 @@ export const dummy_posts = [
   [{
     isbn: "0123456789",
     impression: "まじでめえちゃ面白かった。おすすめできる数少ない本.",
-    user_id: "1"
+    user_id: "1",
+    user_name: "taro"
   }],
   [{
     isbn: "0123456789",
     impression: "この本は刺さる。刺さりすぎて体が穴だらけ。",
-    user_id: "1"
+    user_id: "2",
+    user_name: "pupu"
   }],
   [{
     isbn: "0123456789",
     impression: "今まで読んできた本がクズ紙に見える程よい",
-    user_id: "1"
+    user_id: "3",
+    user_name: "遠山さん"
   }],
   [{
     isbn: "0123456789",
     impression: "この本はマジクソ。",
-    user_id: "1"
+    user_id: "4",
+    user_name: "ぷるぷるプリん"
   }],
   [{
     isbn: "0123456789",
     impression: "おもろかった。.",
-    user_id: "1"
+    user_id: "5",
+    user_name: "ディアルガ"
   }],
 ]
