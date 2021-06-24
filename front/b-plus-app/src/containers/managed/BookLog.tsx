bc6e15019daa83348f3899c92cc7fc124eb07b7c
taro1025 <game4967@gmail.com>
2021-06-24 15:39:48 +0900

    グラフを整えた

--- front/b-plus-app/src/containers/managed/BookLog.tsx
+++ front/b-plus-app/src/containers/managed/BookLog.tsx
@@ -2,12 +2,30 @@ import { Bar } from 'react-chartjs-2';
 
 export const BookLog = () => {
 
+  const date = new Date();
+
+  const today: string = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const fourAgo: string = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const eightAgo: String = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const twelveAgo: String = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const sixTeenAgo: string = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const twentyAgo: string = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const twentyFourAgo: string = (date.getMonth() + 1) + '/' + date.getDate()
+  date.setDate(date.getDate() - 4)
+  const twentyEightAgo: string = (date.getMonth() + 1) + '/' + date.getDate()
+
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: [twentyEightAgo, twentyFourAgo, twentyAgo, sixTeenAgo, twelveAgo, eightAgo, fourAgo, today],
     datasets: [
       {
         label: '読んだ量',
-        data: [200, 452, 671, 792, 900],
+        data: [200, 452, 671, 792, 900, 1000, 1223, 1345],
         backgroundColor: [
           'rgba(6, 95, 227, 1)',
 
@@ -34,7 +52,7 @@ export const BookLog = () => {
         type='bar'
         data={data}
         width={100}
-        height={50}
+        height={100}
         options={options}
       />
     </>
