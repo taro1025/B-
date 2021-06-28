4baacabc10d3d6edbf040b7fa51070a63fa8e1fb
taro1025 <game4967@gmail.com>
2021-06-29 04:09:54 +0900

    ユーザー登録できるようにした

--- front/b-plus-app/src/containers/Login.tsx
+++ front/b-plus-app/src/containers/Login.tsx
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 
 import { NoDecoLink } from '../components/NoDecoLink';
 
+import { createUser } from "../apis/createUser"
 import { login } from '../apis/login';
 import { loginProps } from "../interfaces"
 
@@ -24,6 +25,13 @@ const ButtonWrapper = styled.div`
   margin-top: 15px;
 `;
 
+const InitialButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  padding: 0;
+  appearance: none;
+`
 
 
 
@@ -32,7 +40,9 @@ export const Login = (props: loginProps) => {
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-
+  const [name, setName] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
+  const [isLogin, setDisplay] = useState<boolean>(true)
 
   const handleSubmit = (event: any): void => {
     login({
@@ -43,45 +53,118 @@ export const Login = (props: loginProps) => {
     event.preventDefault()
   }
 
+  const handleSubmitSignUp = (event: any): void => {
+    createUser({
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: passwordConfirmation
+    })
+  }
+
   //開発用ユーザー
   //email    titi#{1=10}@gmail.com
   //password testes1234
   return (
-    <div>
-      <p>ログイン</p>
-      <form>
+    <>
+      {
+        isLogin ?
+          <div>
+            <p>ログイン</p>
+            <form>
 
-        <FormWrapper>
-          <TextFieldWrapper>
-            <TextField
-              id="standard-search"
-              label="Email"
-              name="email"
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-            />
-          </TextFieldWrapper>
-          <TextFieldWrapper>
-            <TextField
-              id="standard-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              name="password"
-              value={password}
-              onChange={event => setPassword(event.target.value)}
-            />
-          </TextFieldWrapper>
-          <ButtonWrapper>
-            <Button variant="contained" color="primary" type="submit"
-              onClick={handleSubmit}>
-              login
+              <FormWrapper>
+                <TextFieldWrapper>
+                  <TextField
+                    id="standard-search"
+                    label="Email"
+                    name="email"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                  />
+                </TextFieldWrapper>
+                <TextFieldWrapper>
+                  <TextField
+                    id="standard-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    name="password"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                  />
+                </TextFieldWrapper>
+                <ButtonWrapper>
+                  <Button variant="contained" color="primary" type="submit"
+                    onClick={handleSubmit}>
+                    login
              </Button>
-            <NoDecoLink to={'/signup'}><p>Sign up?</p></NoDecoLink>
-          </ButtonWrapper>
-        </FormWrapper>
+                  <InitialButton onClick={() => setDisplay(false)}>Sign up?</InitialButton>
+                </ButtonWrapper>
+              </FormWrapper>
 
-      </form>
-    </div>
+            </form>
+          </div>
+          :
+          <div>
+            <p>サインアップ</p>
+            <form>
+
+              <FormWrapper>
+                <TextFieldWrapper>
+                  <TextField
+                    id="standard-search"
+                    label="Name"
+                    name="name"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                  />
+                </TextFieldWrapper>
+                <TextFieldWrapper>
+
+                  <TextField
+                    id="standard-search"
+                    label="Email"
+                    name="email"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                  />
+                </TextFieldWrapper>
+                <TextFieldWrapper>
+                  <TextField
+                    id="standard-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    name="password"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                  />
+                </TextFieldWrapper>
+                <TextFieldWrapper>
+                  <TextField
+                    id="standard-password-input"
+                    label="Password Confirm"
+                    type="password"
+                    autoComplete="current-password"
+                    name="password"
+                    value={passwordConfirmation}
+                    onChange={event => setPasswordConfirmation(event.target.value)}
+                  />
+                </TextFieldWrapper>
+                <ButtonWrapper>
+                  <Button variant="contained" color="primary" type="submit"
+                    onClick={handleSubmitSignUp}>
+                    Sign Up
+             </Button>
+                  <InitialButton onClick={() => setDisplay(true)}>Login?</InitialButton>
+                </ButtonWrapper>
+              </FormWrapper>
+
+            </form>
+          </div>
+
+      }
+    </>
   )
 }
