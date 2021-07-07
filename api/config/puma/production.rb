baf2e9c69441309e03530caf1131b4a028d980b2
taro1025 <game4967@gmail.com>
2021-07-07 19:46:34 +0900

    pumaを変更

--- api/config/puma/production.rb
+++ api/config/puma/production.rb
@@ -1,9 +1,10 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
+#port        ENV.fetch("PORT") { 3000 }
+#environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
+port '3000', '0.0.0.0'
 app_root = File.expand_path("../../..", __FILE__)
 #puts app_root
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
