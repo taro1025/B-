a8419c796ac4ed3709233247903640065b570c36
taro1025 <game4967@gmail.com>
2021-07-07 19:29:46 +0900

    pumaを変更

--- api/config/initializers/session_store.rb
+++ api/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_dhioru', domain: 'フロントエンドのドメイン'
+    Rails.application.config.session_store :cookie_store, key: '_beplus', domain: 'example-1506320326.ap-northeast-1.elb.amazonaws.com', secure: false
 else
     Rails.application.config.session_store :cookie_store, key: '_beplus', secure: false
 end
