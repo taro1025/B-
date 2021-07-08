564ce10967c2babc002431232269bbf327c907de
taro1025 <game4967@gmail.com>
2021-07-08 20:34:23 +0900

    ローカルほすとで通信を試みる

--- front/b-plus-app/src/urls/index.tsx
+++ front/b-plus-app/src/urls/index.tsx
@@ -1,4 +1,4 @@
-const DEFAULT_API_LOCALHOST: string = 'http://127.0.0.1/api/v1'; //'http://127.0.0.1:3000/api/v1';
+const DEFAULT_API_LOCALHOST: string = 'http://127.0.0.1:3000/api/v1'; //'http://127.0.0.1:3000/api/v1';
 
 //Notiifcation
 //Login
