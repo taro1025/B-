00af62d6e4dee18dfe0618140a56d11fa6c20405
taro1025 <game4967@gmail.com>
2021-07-09 23:33:37 +0900

    一度terraform destroyしたのでdbも作り直す

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,7 +6,7 @@ rm -f /app/tmp/pids/server.pid
 # WARNING:createとseedはfargateの初回のみ実行
 # WARNING:タスクを個々に作って実行の方がいいかも
 
-#bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:create RAILS_ENV=production
 bundle exec rails db:migrate RAILS_ENV=production
 #bundle exec rails db:seed RAILS_ENV=production
 
