5bf07704feb8a025c0edb6d6577f5dc531261f9b
taro1025 <game4967@gmail.com>
2021-07-09 21:20:19 +0900

    開発環境でトライ

--- api/config/initializers/session_store.rb
+++ api/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_beplus', domain: 'example-1506320326.ap-northeast-1.elb.amazonaws.com', secure: false
+    Rails.application.config.session_store :cookie_store, key: '_beplus', domain: 'example-769847477.ap-northeast-1.elb.amazonaws.com', secure: false
 else
     Rails.application.config.session_store :cookie_store, key: '_beplus', secure: false
 end
