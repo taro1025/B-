a4eb1a69d0d769f0410ee7ae41ccf150eabab4db
taro1025 <game4967@gmail.com>
2021-07-14 19:08:44 +0900

    developのconfigにautoloader = :classic追加

---
+++ api/app/graphql/types/user_type.rb
@@ -0,0 +1,14 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :email, String, null: false
+    field :password_digest, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :image, String, null: true
+  end
+end
+
+#We could remove any of these fields
+#if we did not want to expose them through the GraphQL API.
