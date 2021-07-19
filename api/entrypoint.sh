0079afe72ef05fd9916c54bec14d29cff58384f1
taro1025 <game4967@gmail.com>
2021-07-20 08:39:25 +0900

    nginxでlocationをgraphql用に設定

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -10,6 +10,6 @@ rm -f /app/tmp/pids/server.pid
 #RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 #RAILS_ENV=production bundle exec rails db:migrate
 #bundle exec rails db:seed RAILS_ENV=production
-bundle install
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
